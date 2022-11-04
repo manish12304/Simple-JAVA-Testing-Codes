ebbb3c65fa8ead1123708c231c6552d955fdecaa
Manish Chauhan <33128830+manish12304@users.noreply.github.com>
2022-11-04 22:57:59 +0530

    Added one new print line when user lose a loterry

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello java");
         System.out.println("1 more Line");
+        System.out.println("2 more Line");
     }
 }
